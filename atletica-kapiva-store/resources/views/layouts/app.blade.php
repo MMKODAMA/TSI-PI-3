73e202338414eb591fad533792aac6529c1e4c19
MATHEUS MAKOTO KODAMA <matheus.mkodama@senacsp.edu.br>
2022-04-27 22:30:53 -0300

    final da aula

--- atletica-kapiva-store/resources/views/layouts/app.blade.php
+++ atletica-kapiva-store/resources/views/layouts/app.blade.php
@@ -5,61 +5,100 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <title>E-commerce</title>
 </head>
 
 <body>
     <header>
-        <nav class="nav navbar-light bg-light navbar-expand-sm">
-            <div class="container-fluid">
-                <ul class="navbar-nav me-auto mb-2">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('product.index') }}">
-                            Produto
+        <nav class="nav navbar-expand-sm d-flex navbar " id="navbar" style="background-color:#FF8000; height:75px">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!--
+                <div class="container-fluid">
+
+            -->
+                <a href=""><img id="logo" src="/img/logo.png"></a>
+                <ul class="navbar-nav ms-auto mb-2 d-flex justify-content-center list-group list-group-horizontal ">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Meu perfil
                         </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('product.index') }}">Produto</a>
+                            <a class="dropdown-item" href="{{ route('product.trash') }}">Lixeira Produto</a>
+                            <a class="dropdown-item" href="{{ route('category.index') }}">Categorias</a>
+                            <a class="dropdown-item" href="{{ route('category.trash') }}">Lixeira Categorias</a>
+                            <a class="dropdown-item" href="{{ route('tag.index') }}">Tags</a>
+                            <a class="dropdown-item" href="{{ route('tag.trash') }}">Lixeira Tags</a>
+
+                            <div class="dropdown-divider"></div>
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <button type="submit">Logout</button>
+                            </form>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
                     </li>
+                    <!--
+                    <ul class="dropdown-menu " aria-labelledby="dropdownMenuButton1">
+                        <li>
+                            <a class="dropdown-item" href="{{ route('product.index') }}">
+                                Produto
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('product.trash') }}">
+                                Lixeira Produto
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('category.index') }}">
+                                Categorias
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('category.trash') }}">
+                                Lixeira Categorias
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('tag.index') }}">
+                                Tags
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('tag.trash') }}">
+                                Lixeira Tags
+                            </a>
+                        </li>
+                        <li class="ml-auto p-2">
+
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <button type="submit">Logout</button>
+                            </form>
+                        </li>
+                    </ul>
+                -->
+
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('product.trash') }}">
-                            Lixeira Produto
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('category.index') }}">
-                            Categorias
+                        <a class="nav-link text-decoration-none " href="{{ route('tag.trash') }}">
+                            Carrinho
                         </a>
                     </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('category.trash') }}">
-                            Lixeira Categorias
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tag.index') }}">
-                            Tags
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tag.trash') }}">
-                            Lixeira Tags
-                        </a>
-                    </li>
-
-                    <li class="ms-auto">
-                        <form action="{{route('logout')}}" method="POST">
-                            @csrf
-                            <button type="submit">Logout</button>
-                        </form>
-                    </li>
 
                 </ul>
+                <!-- add
+                        </div>
+                    </div>
+                -->
+
             </div>
         </nav>
+
     </header>
     <main>
         <div>
